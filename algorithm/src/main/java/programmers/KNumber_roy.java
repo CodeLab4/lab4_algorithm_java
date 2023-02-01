05c8282baa8bc49a3b01e716a1631ec890f15d3b
lvalentine6 <ysr1227@naver.com>
2023-02-01 21:01:59 +0900

    Feat : "프로그래머스 k번째수_로이"

--- algorithm/src/main/java/programmers/KNumber_roy.java
+++ algorithm/src/main/java/programmers/KNumber_roy.java
@@ -6,8 +6,8 @@ public class KNumber_roy {
     public static void main(String[] args) {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-        int[] answer = new int[commands.length];
 
+        int[] answer = new int[commands.length];
         for (int i = 0; i < commands.length; i++) {
             answer[i] = Arrays.stream(array, commands[i][0] - 1, commands[i][1])
                     .sorted()
