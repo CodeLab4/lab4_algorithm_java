8673d25e738e43d02daa35fbee46b86bb18d03ab
birdieHyun <flaehdan@gmail.com>
2023-02-21 21:25:06 +0900

    feat : 프로그래머스 소수찾기 - 파이썬
    Body :

---
+++ algorithm/src/main/java/programmers/PrimeNumber_Birdie.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class PrimeNumber_Birdie {
+}
+
+/**
+ * import math
+ *
+ * def solution(n):
+ *     answer = 0
+ *
+ *     for i in range(2, n + 1):
+ *         if checkPrime(i) == True:
+ *             answer += 1
+ *
+ *     return answer
+ *
+ *
+ * def checkPrime(n):
+ *     for i in range(2, int(math.sqrt(n)) + 1):
+ *         if n % i == 0:
+ *             return False
+ *
+ *     return True
+ */
