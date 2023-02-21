823a357941e67b0132a857eb9147ec952ede4ace
birdieHyun <flaehdan@gmail.com>
2023-02-21 21:56:46 +0900

    feat : 프로그래머스 과일 장수 - 파이썬
    Body :

---
+++ algorithm/src/main/java/programmers/FruitSeller_Birdie.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class FruitSeller_Birdie {
+}
+
+/**
+ * def solution(k, m, score):
+ *     answer = 0
+ *     score = sorted(score, reverse=True)
+ *
+ *     for i in range(0, len(score), m):
+ *         tmp = score[i:i + m]
+ *
+ *         if len(tmp) == m:
+ *             answer += min(tmp) * m
+ *
+ *     return answer
+ */
