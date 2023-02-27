43d939985406e8a85e4360fa103fa6b093048c96
lvalentine6 <ysr1227@naver.com>
2023-02-28 00:25:10 +0900

    Feat : "콜라_로이"

---
+++ algorithm/src/main/java/baekjoon/B9465_roy.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B9465_roy {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bufferedReader.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(bufferedReader.readLine());
+            int[][] arr = new int[2][n];
+
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[0][j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[1][j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+
+            System.out.println(Arrays.deepToString(arr));
+        }
+    }
+}
