5b873267649f7c04d72a5ab10930f03f3690f25f
lvalentine6 <ysr1227@naver.com>
2023-04-08 17:49:07 +0900

    프로그래머스 옹알이 (2) roy

--- algorithm/src/main/java/programmers/lv1/Babbling_2_roy.java
+++ algorithm/src/main/java/programmers/lv1/Babbling_2_roy.java
@@ -5,6 +5,22 @@ public class Babbling_2_roy {
         String[] babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
         int answer = 0;
 
+        String[] arr = {"aya", "ye", "woo", "ma"};
+
+        for (int i = 0; i < babbling.length; i++) {
+            String temp = babbling[i];
+            String lastWord = "";
+            for (int j = 0; j < arr.length; j++) {
+                if (!arr[j].equals(lastWord) && temp.startsWith(arr[j])) {
+                    temp = temp.substring(arr[j].length());
+                    lastWord = arr[j];
+                    j = -1;
+                }
+            }
+            if(temp.equals("")) {
+                answer++;
+            }
+        }
         System.out.println(answer);
     }
 }
