d0d6a616f564a7b9a322dc9575f9191df3c58a2d
lvalentine6 <ysr1227@naver.com>
2023-04-08 15:31:20 +0900

    Docs : 옹알이2 init

---
+++ algorithm/src/main/java/programmers/lv1/Babbling_2_roy.java
@@ -0,0 +1,10 @@
+package programmers.lv1;
+
+public class Babbling_2_roy {
+    public static void main(String[] args) {
+        String[] babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        int answer = 0;
+
+        System.out.println(answer);
+    }
+}
