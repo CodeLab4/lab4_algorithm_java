3a2b69a9672320e1876728bea2b5ed65a82649dd
kimdongmin <meena2003@naver.com>
2023-04-08 17:11:59 +0900

    프로그래머스 옹알이 (2) 코어 [1]

---
+++ algorithm/src/main/java/programmers/lv1/babbling_2_core.java
@@ -0,0 +1,29 @@
+package z.programmers.lv1;
+
+public class babbling2_core {
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"}));
+    }
+
+    public static int solution(String[] babbling) {
+        String[] canSpeak = {"aya", "ye", "woo", "ma"};
+        int count = 0;
+
+        for (int i = 0; i < babbling.length; i++) { // 옹알이 하나씩 탐색
+            String word = babbling[i];
+            String preWord = ""; // 이전에 확인한 옹알이
+            for (int j = 0; j < canSpeak.length; j++) { // 말하기 가능한 단어 비교
+                if (!canSpeak[j].equals(preWord) && word.startsWith(canSpeak[j])) { // 직전에 확인한 단어와 같지 않고, 발음 가능한 옹알이와 같다면
+                    word = word.replaceFirst(canSpeak[j], "");
+                    preWord = canSpeak[j];
+                    j = -1;
+                }
+            }
+            if (word.equals("")) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
