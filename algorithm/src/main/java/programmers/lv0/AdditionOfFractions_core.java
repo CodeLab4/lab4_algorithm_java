6769a7084ecc181fe3381d5a7059be16534bdc72
kimdongmin <meena2003@naver.com>
2023-04-12 11:26:58 +0900

    프로그래머스 분수의 덧셈 core [1]

---
+++ algorithm/src/main/java/programmers/lv0/AdditionOfFractions_core.java
@@ -0,0 +1,26 @@
+package programmers.lv0;
+
+public class AdditionOfFractions_core {
+    public static void main(String[] args) {
+        System.out.println(solution(1, 2, 3, 4));
+    }
+
+    public static int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] result = new int[2];
+        int top = numer1 * denom2 + numer2 * denom1;
+        int bottom = denom1 * denom2;
+
+        int gcd = gcd(top, bottom);
+        result[0] = top / gcd;
+        result[1] = bottom / gcd;
+
+        return result;
+    }
+
+    public static int gcd(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return gcd(num2, num1 % num2);
+    }
+}
