588295aadf05707d484053054bd903a56cd447ae
Hana <dokkisan.wang@gmail.com>
2023-04-13 14:59:03 +0900

    프로그래머스 평균 구하기 Hana[3]

---
+++ algorithm/src/main/java/programmers/lv1/AverageCalculator_Hana.java
@@ -0,0 +1,22 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+
+public class AverageCalculator_Hana {
+    public static void main(String[] args) {
+//        int[] arr = {1,2,3,4};
+        int[] arr = {5,5};
+
+        System.out.println(solution(arr));
+    }
+
+    public static double solution(int[] arr) {
+        double answer = 0;
+
+        answer = Arrays.stream(arr)
+                .average()
+                .orElse(0);
+
+        return answer;
+    }
+}
