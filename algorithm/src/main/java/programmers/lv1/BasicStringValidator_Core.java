d17c82fea8fa55cbfd7246aa002ba3d917f679f1
kimdongmin <meena2003@naver.com>
2023-04-26 12:18:03 +0900

    프로그래머스 문자열 다루기 기본 Core [2]

---
+++ algorithm/src/main/java/programmers/lv1/BasicStringValidator_Core.java
@@ -0,0 +1,20 @@
+package programmers.lv1;
+
+public class BasicStringValidator_Core {
+    public static void main(String[] args) {
+        System.out.println(solution("23a4"));
+    }
+
+    public static boolean solution(String s) {
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (Character.getNumericValue(c) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
