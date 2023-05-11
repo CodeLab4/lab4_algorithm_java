e27c8b1cbeca315cdb26a56e12b25d2870893d01
Hana <dokkisan.wang@gmail.com>
2023-05-11 18:54:16 +0900

    프로그로머스 문자열 내림차순으로 배치하기 Hana[1]

---
+++ algorithm/src/main/java/programmers/lv1/StringDescendingSorter.java
@@ -0,0 +1,23 @@
+package programmers.lv1;
+
+import java.util.*;
+
+/**
+ * 프로그래머스 문자열 내림차순으로 배치하기
+ */
+public class StringDescendingSorter {
+    public static void main(String[] args) {
+        String s = "Zbcdefg";
+        System.out.println(solution(s));
+    }
+
+    public static String solution(String s) {
+        String answer = "";
+
+        char[] temp = s.toCharArray();
+        Arrays.sort(temp);
+        answer = new StringBuilder(new String(temp)).reverse().toString();
+
+        return answer;
+    }
+}
