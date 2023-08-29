a83f3d66175cb66570a41d77af21c32591b56501
lvalentine6 <ysr1227@naver.com>
2023-08-29 17:41:41 +0900

    프로그래머스 과제 진행하기 roy [1]

--- algorithm/src/main/java/programmers/lv2/Homework_roy.java
+++ algorithm/src/main/java/programmers/lv2/Homework_roy.java
@@ -1,20 +1,19 @@
 package programmers.lv2;
 
-import java.sql.Time;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Homework_roy {
 	public static void main(String[] args) {
-		String[][] plans = {{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"}, {"computer", "12:30", "100"}};
+		String[][] plans = {{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"},
+			{"computer", "12:30", "100"}};
 		System.out.println(Arrays.toString(solution(plans)));
 	}
 
@@ -22,21 +21,27 @@ public class Homework_roy {
 		String[] answer = new String[plans.length];
 		Map<String, LocalTime> workMap = new HashMap<>();
 		Map<String, Integer> workTime = new HashMap<>();
+		List<Integer> answerList = new ArrayList<>();
 
 		for (String[] s : plans) {
 			int[] timeTemp = Arrays.stream(s[1].split(":")).mapToInt(Integer::parseInt).toArray();
 			workMap.put(s[0], LocalTime.of(timeTemp[0], timeTemp[1]));
+			workTime.put(s[0], Integer.parseInt(s[2]));
 		}
 
 		List<String> list = new ArrayList<>(workMap.keySet());
 		list.sort(Comparator.comparing(workMap::get));
 
-		for (String s : list) {
+		Deque<String> deque = new ArrayDeque<>(list);
 
+		while (!deque.isEmpty()) {
+			String temp = deque.poll();
+			LocalTime plusTime = workMap.get(temp).plusMinutes(workTime.get(temp));
+			LocalTime nextWorkTime = workMap.get(deque.peek());
+			if (plusTime.isAfter(nextWorkTime)) {
+				LocalTime m = LocalTime.ofSecondOfDay(plusTime.toSecondOfDay() - nextWorkTime.toSecondOfDay());
+			}
 		}
-
-		System.out.println(workMap);
-		System.out.println(list);
 
 		return answer;
 	}
