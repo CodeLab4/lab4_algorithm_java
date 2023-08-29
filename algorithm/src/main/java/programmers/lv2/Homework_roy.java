45e0ddbf4b1b2e7aaa2e4838980509e0775ccac7
lvalentine6 <ysr1227@naver.com>
2023-08-29 20:38:27 +0900

    프로그래머스 과제 진행하기 roy [1]

--- algorithm/src/main/java/programmers/lv2/Homework_roy.java
+++ algorithm/src/main/java/programmers/lv2/Homework_roy.java
@@ -1,14 +1,12 @@
 package programmers.lv2;
 
 import java.time.LocalTime;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Homework_roy {
 	public static void main(String[] args) {
@@ -18,31 +16,51 @@ public class Homework_roy {
 	}
 
 	public static String[] solution(String[][] plans) {
-		String[] answer = new String[plans.length];
-		Map<String, LocalTime> workMap = new HashMap<>();
-		Map<String, Integer> workTime = new HashMap<>();
-		List<Integer> answerList = new ArrayList<>();
+		Map<LocalTime, String> workFlow = new HashMap<>();
+		Map<String, LocalTime> workTime = new HashMap<>();
+		List<String> answerList = new ArrayList<>();
+		PriorityQueue<LocalTime> queue = new PriorityQueue<>();
+		PriorityQueue<LocalTime> restQueue = new PriorityQueue<>();
 
 		for (String[] s : plans) {
-			int[] timeTemp = Arrays.stream(s[1].split(":")).mapToInt(Integer::parseInt).toArray();
-			workMap.put(s[0], LocalTime.of(timeTemp[0], timeTemp[1]));
-			workTime.put(s[0], Integer.parseInt(s[2]));
+			LocalTime instance = LocalTime.parse(s[1]);
+			workFlow.put(instance, s[0]);
+			workTime.put(s[0], LocalTime.ofSecondOfDay(Integer.parseInt(s[2]) * 60L));
+			queue.add(instance);
 		}
 
-		List<String> list = new ArrayList<>(workMap.keySet());
-		list.sort(Comparator.comparing(workMap::get));
+		// 1. 숙제를 시간순으로 정렬
+		// 2. 현재 숙제에 시간을 더한값과 다음 숙제의 시간을 비교
+		// 3. 작으면 레디큐에서 뺴서 진행 후 다시 비교 같으면 그냥 진행
+		// 4. 많으면 차이만큼 맵에 저장하고 남은 시간 레디큐에 넣기
 
-		Deque<String> deque = new ArrayDeque<>(list);
-
-		while (!deque.isEmpty()) {
-			String temp = deque.poll();
-			LocalTime plusTime = workMap.get(temp).plusMinutes(workTime.get(temp));
-			LocalTime nextWorkTime = workMap.get(deque.peek());
-			if (plusTime.isAfter(nextWorkTime)) {
-				LocalTime m = LocalTime.ofSecondOfDay(plusTime.toSecondOfDay() - nextWorkTime.toSecondOfDay());
+		while (!queue.isEmpty()) {
+			LocalTime nowWork = queue.poll();
+			String subject = workFlow.get(nowWork);
+			LocalTime restTime = workTime.get(subject);
+			if (queue.size() > 1) {
+				LocalTime nextWork = queue.peek();
+				LocalTime difTime = LocalTime.ofSecondOfDay(
+					nextWork.toSecondOfDay() - nowWork.toSecondOfDay() + restTime.toSecondOfDay());
+				if (difTime.toSecondOfDay() < 0) {
+					restQueue.add(difTime);
+					workTime.replace(subject, difTime);
+				} else if (difTime.toSecondOfDay() == 0){
+					answerList.add(subject);
+				} else {
+					if(!restQueue.isEmpty()) {
+						restQueue.poll();
+					}
+					workTime.replace(subject, LocalTime.of(0,0));
+					answerList.add(subject);
+				}
 			}
 		}
-
-		return answer;
+		System.out.println(queue);
+		System.out.println(restQueue);
+		System.out.println(workFlow);
+		System.out.println(workTime);
+		System.out.println(answerList);
+		return new String[] {"s"};
 	}
 }
